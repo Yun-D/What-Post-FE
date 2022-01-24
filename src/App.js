8055db171a60f04bf75828d0cc1f4b3be5200cc3
Yun-D <46706905+Yun-D@users.noreply.github.com>
2022-01-24 20:04:46 +0900

    ✨ 네비게이션바 기능 완성 (#3)
    * ✨ navBar 파일 생성
    
    * 💄 navBar style 생성
    
    * ✨💄 반응형 구현, css 추가
    
    * 💄 메뉴 아이콘 및 드로어 부분 align 맞춤, 로고 영역 고정
    
    * 💄 로고부분 굵게 처리, 아이콘 사이 간격 조정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import NavBar from './Components/navigation/NavBar';
+import Home from './Components/Home/Home';
+import MyPost from './Components/MyPost/MyPost';
+import SearchBook from './Components/SearchBook/SearchBook';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element = { <Home /> } />
+          <Route path="/my_post" element = { <MyPost /> } />
+          <Route path="/search_book" element = { <SearchBook /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
---
+++ src/Components/MyPost/MyPost.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const MyPost = () => {
+    return (
+        <>
+        MyPost 내 포스트
+        </>
+    );
+}
+
+export default MyPost;
