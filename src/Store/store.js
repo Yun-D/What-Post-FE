90375ec8e1d68452dd299f3ae46aa82c2ac206ef
Yun-D <yund_272@naver.com>
2022-09-13 19:55:20 +0900

    ✨Feat: 영화 검색용 리듀서 생성

--- src/Store/store.js
+++ src/Store/store.js
@@ -32,11 +32,41 @@ export const bookSearchSlice = createSlice({
   },
 });
 
+export const movieSearchSlice = createSlice({
+  name: "movieSearch",
+  initialState: {
+    query: "",
+    items: [], //title, link, image, subtitle, pubDate, director, actor, userRating 포함
+    start: 1, //결과 페이지 번호
+  },
+  reducers: {
+    m_setQuery: (state, action) => {
+      //검색어 저장(검색 쿼리로 들어갈 값)
+      state.query = action.payload;
+    },
+    m_setItems: (state, action) => {
+      //영화 검색 결과 기록 저장
+      state.items = action.payload;
+    },
+    m_nextPage: (state) => {
+      //더보기 버튼 눌러 다음페이지로
+      state.start += 10;
+    },
+    m_setPage: (state, action) => {
+      state.start = action.payload;
+    },
+  },
+});
+
 export const { setSearch, setQuery, setBooks, nextPage, setPage, isEndPage } =
   bookSearchSlice.actions;
+
+export const { m_setQuery, m_setItems, m_nextPage, m_setPage } =
+  movieSearchSlice.actions;
 
 export default configureStore({
   reducer: {
     bookSearch: bookSearchSlice.reducer,
+    movieSearch: movieSearchSlice.reducer,
   },
 });
