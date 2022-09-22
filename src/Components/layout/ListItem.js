af87282ccc20e1295bf15481d68da6ec78a91479
Yun-D <yund_272@naver.com>
2022-09-22 15:23:25 +0900

    🗑️Remove: css-in-js로 변경하기 위해 코드 삭제, ListItem.js로 코드 옮김

--- src/Components/layout/ListItem.js
+++ src/Components/layout/ListItem.js
@@ -1,53 +1,76 @@
 import React from "react";
-import "./ListItem.css";
+
 import theme from "../../Styles/theme";
+import styled from "styled-components";
 
 import { StyledLink } from "../etc/StyledLink";
 
 /////////////////////////////// 책 검색 결과를 리스트형태로 보여주는 컴포넌트
 const ListItem = (props) => {
   return (
-    <li className="list_li">
+    <ItemCard className="rowDirection">
       <dl>
         <dd>
           <p>{props.is_end}</p>
         </dd>
       </dl>
-      <dl className="list_dl">
-        <img
-          src={props.thumbnail}
-          alt={props.thumbnail}
-          width="auto"
-          height="auto"
-        />
+      <img
+        src={props.thumbnail}
+        alt={props.thumbnail}
+        width="auto"
+        height="auto"
+      />
 
-        <dd className="list_dd">
-          <StyledLink
-            to={props.tolink}
-            state={{
-              thumbnail: props.thumbnail,
-              title: props.title,
-              authors: props.authors,
-              publisher: props.publisher,
-              publishDate: props.datetime,
-              contents: props.contents,
-            }}
-            decoration_color={`${theme.colors.peacock}`}
-            decoration_thckness="2px"
-            onClick={props.onClick}
-          >
-            <h3 className="title">{props.title}</h3>
-          </StyledLink>
+      <ContentsDiv>
+        <StyledLink
+          to={props.tolink}
+          state={{
+            thumbnail: props.thumbnail,
+            title: props.title,
+            authors: props.authors,
+            publisher: props.publisher,
+            publishDate: props.datetime,
+            contents: props.contents,
+          }}
+          color={`${theme.colors.peacock}`}
+          decoration_color={`${theme.colors.peacock}`}
+          decoration_thckness="2px"
+          onClick={props.onClick}
+        >
+          <h3>{props.title}</h3>
+        </StyledLink>
 
-          <h4 className="authors">{props.authors}</h4>
-          <br />
-          <p className="smallText">
-            {props.publisher} | {props.datetime}
-          </p>
-        </dd>
-      </dl>
-    </li>
+        <H4 className="authors">{props.authors}</H4>
+        <br />
+        <SmallTxt>
+          {props.publisher} | {props.datetime}
+        </SmallTxt>
+      </ContentsDiv>
+    </ItemCard>
   );
 };
+
+const ItemCard = styled.div`
+  width: 100%;
+  list-style: none;
+  border: 0.5px solid #bbbbbb;
+  padding: 20px 30px;
+  margin-bottom: 4px;
+  background-color: white;
+  border-radius: 8px;
+`;
+const ContentsDiv = styled.div`
+  flex: 1;
+  margin-left: 30px;
+  margin-bottom: 10px;
+`;
+
+const H4 = styled.h4`
+  color: rgb(97, 97, 97);
+`;
+const SmallTxt = styled.p`
+  color: gray;
+  font-size: 0.75rem;
+`;
 
 export default ListItem;
