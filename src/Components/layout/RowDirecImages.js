676ef126a2f60f807f0ae7b55a039d76f70cbec9
Yun-D <yund_272@naver.com>
2022-11-23 22:04:50 +0900

    🎨Design: 캐러셀 스타일 적용

--- src/Components/layout/RowDirecImages.js
+++ src/Components/layout/RowDirecImages.js
@@ -1,20 +1,48 @@
 import React from "react";
 import styled from "styled-components";
+import theme from "Styles/theme";
 
 const RowDirecImages = (props) => {
   return (
-    <ItemCard>
-      <img src={props.thumbnail} alt={props.title} />
-      <p>{props.title}</p>
-    </ItemCard>
+    <ContainerDiv>
+      <ItemCard>
+        <ItemIMG src={props.thumbnail} alt={props.thumbnail} />
+        <ItemP>{props.title}</ItemP>
+      </ItemCard>
+    </ContainerDiv>
   );
 };
 
+const ContainerDiv = styled.ul`
+  display: inline-block;
+  white-space: nowrap;
+  align-items: center;
+  justify-content: center;
+`;
 const ItemCard = styled.div`
   flex-direction: column;
   background-color: white;
-  border-radius: 8px;
-  margin-right: 2%;
+  width: 170px;
+  white-space: pre-line;
+  overflow: hidden;
+`;
+
+const ItemIMG = styled.img`
+  width: 150px;
+  box-shadow: ${theme.size.boxShadow};
+  margin-bottom: 5%;
+  border-radius: 5px;
+`;
+const ItemP = styled.p`
+  font-size: ${theme.textSize.postContents};
+  width: 150px;
+  height: 40px;
+
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 `;
 
 export default RowDirecImages;
--- src/Pages/SearchBook/BestSeller.js
+++ src/Pages/SearchBook/BestSeller.js
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getBestSeller } from "APIs/api";
 import keys from "APIs/api_key";
 import RowDirecImages from "Components/layout/RowDirecImages";
 import styled from "styled-components";
+import LeftIcon from "@material-ui/icons/ChevronLeft";
+import RightIcon from "@material-ui/icons/ChevronRight";
 
 const BestSeller = (props) => {
   const [bestSeller, setBestSeller] = useState([]);
+  const [carouselCount, setCarouselCount] = useState(0);
+
+  const moveButton = useRef();
 
   useEffect(() => {
     getBestSellerHandler(props.categoryID);
@@ -22,21 +27,49 @@ const BestSeller = (props) => {
     setBestSeller(data.item);
   };
 
+  const carouselHandler = () => {
+    moveButton.current.style.transform = `translateX(-30%)`;
+  };
+
   return (
-    <ItemArea className="rowDirection">
-      {bestSeller.map((book, idx) => (
-        <RowDirecImages
-          key={idx}
-          thumbnail={book.coverLargeUrl}
-          title={book.title}
-          author={book.author}
-          publisher={book.publisher}
-        />
-      ))}
-    </ItemArea>
+    <Div>
+      <ItemArea className="rowDirection">
+        <ULarea ref={moveButton}>
+          {bestSeller.map((book, idx) => (
+            <RowDirecImages
+              key={idx}
+              thumbnail={book.coverLargeUrl}
+              title={book.title}
+              author={book.author}
+              publisher={book.publisher}
+            />
+          ))}
+        </ULarea>
+      </ItemArea>
+
+      <ButtonArea>
+        <MoveBtn onClick={carouselHandler}>
+          <LeftIcon fontSize="large" />
+        </MoveBtn>
+
+        <Blank />
+        <MoveBtn>
+          <RightIcon fontSize="large" />
+        </MoveBtn>
+      </ButtonArea>
+    </Div>
   );
 };
 
+const Div = styled.div`
+  height: 100%;
+  position: relative;
+`;
+const Blank = styled.div`
+  flex: 999;
+  width: 90%;
+  height: 100%;
+`;
 const ItemArea = styled.div`
   width: 100%;
   border: 0.5px solid #bbbbbb;
@@ -44,6 +77,29 @@ const ItemArea = styled.div`
   margin-bottom: 4px;
   background-color: white;
   border-radius: 8px;
+`;
+const ButtonArea = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 40%;
+  padding: 1%;
+  display: flex;
+`;
+const ULarea = styled.ul`
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+const MoveBtn = styled.button`
+  height: 52px;
+  width: 52px;
+  border-radius: 50px;
+  background-color: #6badb6cc;
+
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default BestSeller;
