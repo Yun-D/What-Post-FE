d31925704c5361349c12ad83f7dbafd63d299f5f
Yun-D <yund_272@naver.com>
2023-06-09 15:18:39 +0900

    ✨Feat: 통합 검색주제 선택 select box 구현

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 import { SearchBar } from "Components/etc/SearchBar";
 import BoxOffice from "Pages/SearchMovie/BoxOffice";
+import styled from "styled-components";
 
 const Home = () => {
+  const [selected, setSelected] = useState("");
+
+  const selectList = [
+    { value: "post", name: "포스트 검색" },
+    { value: "user", name: "유저 검색" },
+  ];
+  const selectHandle = (e) => {
+    setSelected(e.target.value);
+  };
+
   return (
     <div className="contents_div">
-      <div>
+      <div className="rowDirection">
+        <StyledSelect onChange={selectHandle} value={selected}>
+          {selectList.map((item) => (
+            <option value={item.value} key={item.value}>
+              {item.name}
+            </option>
+          ))}
+        </StyledSelect>
         <SearchBar />
       </div>
       <div>
@@ -18,5 +37,13 @@ const Home = () => {
     </div>
   );
 };
+
+const StyledSelect = styled.select`
+  width: 16vh;
+  height: 45px;
+  border-radius: 8px;
+  border: gray solid 0.5px;
+  margin-right: 0.5%;
+`;
 
 export default Home;
